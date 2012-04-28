f94998508848e25ab547c14f7b23d6297425e879
vins31 <vincentweb31@gmail.com>
2012-04-28 16:07:18 +0200

    * ajout du package mathutils * tests unitaires du module geometry

--- ia/robot.py
+++ ia/robot.py
@@ -1,13 +1,14 @@
 # -*-coding:UTF-8 -*
+
+from mathutils.types import Vertex 
+
 class Robot:
     def __init__(self, x, y, theta, dim_l, dim_r, dim_t, dim_b):
-        self.x = x # 10e de mm
-        self.y = y # 10e de mm
+        self.pos = Vertex(x, y) # 10e de mm
         self.theta = theta # centidegree
 
         self.action = None #forwarding, rotating 
-        self.x_target = None # à court terme
-        self.y_target = None
+        self.pos_target = None # à court terme
         self.theta_target = None
 
         # Dimmensions du robot, left, right, bottom, top
@@ -34,6 +35,6 @@ class Robot:
 
 
     def __str__(self):
-        return "x= %.3fm, y= .3fm, theta=%.2f" % (self.x/10000, self.y/10000, self.theta/100)
+        return "x=%.3fm, y=%.3fm, theta=%.2f" % (self.x/10000, self.y/10000, self.theta/100)
 
 
