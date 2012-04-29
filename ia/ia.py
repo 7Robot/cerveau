15b0e6155e43c8f6ac719d2372374f34d2b9b573
Élie Bouttier <cube.elie@gmail.com>
2012-04-29 12:06:23 +0200

    Suppr. vieux coms + defaut ip: r2d2,mission: tests

--- ia/ia.py
+++ ia/ia.py
@@ -3,23 +3,13 @@
 
 import socket
 
-
-
-#sock.setblocking(False)
-
-#cmd = sock.makefile().readline()
-#print("«"+cmd+"»")
-
-#sock.send(b"rot -90\n")
-
-
 from can import Can
 from robot import Robot
 from event_dispatcher import Event_dispatcher
 
 class IA:
-	def __init__(self, mission_prefix, ip, port=7773):
-		assert(mission_prefix in ["petit", "grand"])
+	def __init__(self, mission_prefix, ip="r2d2", port=7773):
+		assert(mission_prefix in ["petit", "grand", "tests"])
 		self.mission_prefix = mission_prefix
 		self.ip   = ip
 		self.port = port
@@ -68,16 +58,6 @@ class IA:
 		self.sock.shutdown(socket.SHUT_WR) 
 		self.sock.close()
 
-#m.process_event("plop")
-#m.actions[0]("plop")
-#try:
-#	print(m.actions[1])
-#except KeyError:
-#	pass
-
-#print(state)
-
-
 if __name__ == "__main__":
-	ia = IA("petit", "r2d2")
+	ia = IA("tests", "r2d2")
 	ia.main()
