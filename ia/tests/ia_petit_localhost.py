a18e64eb70db14845994e9a2824d19b9c175c234
vins31 <vincentweb31@gmail.com>
2012-05-11 00:22:34 +0200

    ajout des fichiers yml

--- ia/tests/ia_petit_localhost.py
+++ ia/tests/ia_petit_localhost.py
@@ -6,7 +6,9 @@ Created on 10 mai 2012
 from ia import IA
 
 
-
+m = __import__("mathutils")
+print(m.__dict__.keys())
+print(getattr(m, "types"))
 
 if __name__ == "__main__":
     ia = IA("petit", **{"can_ip" : "localhost", "ui_ip": "localhost"})
