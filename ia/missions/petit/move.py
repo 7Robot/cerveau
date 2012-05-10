a9c22c7a2e9c4d1ceb960167c9a518f6908c4c60
Élie Bouttier <cube.elie@gmail.com>
2012-05-11 01:10:48 +0200

    Début mission move (ajout forward pour le moment)

---
+++ ia/missions/petit/move.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 30 avr. 2012
+
+Je pense qu'il vaut mieux préfixer les modules par _missions 
+car sinon on a des import automatiques de missions qui ont le même nom
+'''
+
+# -*-coding:UTF-8 -*
+
+from missions.mission import Mission
+from mathutils.types import Vertex
+from math import cos, sin, pi, copysign
+
+class MoveMission(Mission):
+    def __init__(self, robot):
+        super(self.__class__,self).__init__(robot)
+        
+        # opération en cours d'execution
+        # valeurs possibles :
+        # * None
+        # * stopping
+        self.state = None # pas d'opération en cours
+
+        # mission en cours
+        # valeur possible :
+        # * None
+        # * forward
+        self.mission = None # pas de mission en cours
+        
+        # dernière position connu du robot
+        self.pos = self.robot.pos # position initial
+        self.rot = self.robot.rot # orientation initial
+
+        # position demandé du robot
+        # initialement, on est à priori là où on veut être
+        self.target_pos = self.robot.pos
+        self.target_rot = self.robot.rot
+
+    # avancer d'une distance donné
+    def forward(self, callback, dist):
+        self.callback = callback
+        self.mission = "forward"
+        self.dist = dist
+        self.stop()
+
+    # stopper la mission en cours
+    def stop(self):
+        if self.mission == None:
+            self.process_event(MoveDoneEvent())
+        elif self.mission == "forward":
+            self.state = "stopping"
+            self.missions["forward"].stop()
+
+    def process_event(self, event):
+        # events gérés quelque soit l'état
+        if event.name == "odo":
+            if event.type == "pos":
+                # màj de la position actuelle
+                self.pos = event.pos
+                self.rot = event.rot
+
+        # events gérés suivant la mission en cours
+        if self.mission == "forward": # mission d'avancement
+            if event.name == "movedone": # la mission précédente est terminé
+                self.target_pos = self.pos
+                        + Vertex(self.dist * cos(self.rot/18000*pi),
+                                self.dist * sin(self.rot/18000*pi))
+                self.state = "forwarding"
+                self.missions["forward"].start()
+            elif event.name == "forwarddone": # arrêt
+                if self.state == "forwarding":
+                    # l'avancement est terminé
+                    self.state = None
+                    self.mission = None
+                    self.callback.process_event(MoveDoneEVent())
+                elif self.state == "stopping":
+                    # l'arrêt de l'oppération est terminé
+                    self.process_event(MoveDoneEvent())
