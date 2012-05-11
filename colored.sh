33087ef622ae24bf9967f1ca9702ef676ff1c081
vins31 <vincentweb31@gmail.com>
2012-05-11 23:16:09 +0200

    * Ajout de la coloration pour les logs : On voit MIEUX les ERRORS * tests de la mission de calibration

--- colored.sh
+++ colored.sh
@@ -4,7 +4,18 @@
 # cat log_file | ./colored.sh
 
 while read line; do
-    if `echo $line | grep -iq 'odo'` ; then
+    # Logging
+    if `echo $line | grep -q 'CRITIC'` ; then
+        printf "\033[1;41;33m%s\033[0m\n" "$line"
+    elif `echo $line | grep -q 'ERROR'` ; then
+        printf "\033[1;41;37m%s\033[0m\n" "$line"
+    elif `echo $line | grep -q 'WARNING'` ; then
+        printf "\033[1;43;30m%s\033[0m\n" "$line"
+    elif `echo $line | grep -q 'INFO'` ; then
+        printf "\033[1;40;37m%s\033[0m\n" "$line"
+
+    # Can messages
+    elif `echo $line | grep -iq 'odo'` ; then
         printf "\033[1;40;37m%s\033[0m\n" "$line"
     elif `echo $line | grep -iq 'asserv'` ; then
         printf "\033[1;42;30m%s\033[0m\n" "$line"
@@ -16,6 +27,10 @@ while read line; do
         printf "\033[1;46;32m%s\033[0m\n" "$line"
     elif `echo $line | grep -iq 'turret'` ; then
         printf "\033[1;44;32m%s\033[0m\n" "$line"
+    
+
+
+
     else
         echo $line
     fi
