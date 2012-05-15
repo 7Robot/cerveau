931f6ff841436dd33d813878041cb7c68bec56eb
vins31 <vincentweb31@gmail.com>
2012-05-15 10:42:14 +0200

    move corrigé

--- ia/missions/common/move.py
+++ ia/missions/common/move.py
@@ -15,152 +15,129 @@ class MoveMission(Mission):
     def __init__(self, robot, can, ui):
         super(self.__class__,self).__init__(robot, can, ui)
          
-        # dernire position connu du robot
-        # determin soit par l'odo, soit par le biais connu du robot
-        self._pos = self.robot.pos # position initial
-        self._rot = self.robot.rot # orientation initial
+        self.mission = None # pas de mission en cours
 
-        # position demand du robot
-        # initialement, on est  priori l o on veut tre
-        self._target_pos = self.robot.pos
-        self._target_rot = self.robot.rot
+        self._fonction = None
 
-        self.odo = None # pas de recalibration en cours
-        
-        '''
-        opration en cours d'execution
-        valeurs possibles :
-        * None
-        * running
-        * stopping (demande d'arrt de speed)
-        '''
-        self.state = None # pas d'opration en cours
+        self.value = {}
 
-        '''
-        mission en cours
-        valeur possible :
-        * None
-        * forward
-        * rotate
-        * speed
-        '''
-        self._mission = None # pas de mission en cours
+    def _set_fonction(self, fonction):
+        self.logger.info("[fonction] %s -> %s" %(self._fonction, fonction))
+        self._fonction = fonction
+    def _get_fonction(self):
+        return self._fonction
+    fonction = property(_get_fonction, _set_fonction)
 
-    def _set_pos(self, pos):
-        self.logger.debug("[real] pos: %d %d" %(pos.x, pos.y))
-        self._pos = pos
-    def _get_pos(self):
-        return self._pos
-    pos = property(_get_pos, _set_pos)
+    ### FONCTIONS DISPONIBLE ###
+    # Elle demmare sur un odo pos pour avoir une valeur a jour
 
-    def _set_target_pos(self, target_pos):
-        self.logger.info("[target] pos: %d %d" %(target_pos.x, target_pos.y))
-        self._target_pos = target_pos
-    def _get_target_pos(self):
-        return self._target_pos
-    target_pos = property(_get_target_pos, _set_target_pos)
-
-    def _set_rot(self, rot):
-        self.logger.debug("[real] rot: %d" %rot)
-        self._rot = rot
-    def _get_rot(self):
-        return self._rot
-    rot = property(_get_rot, _set_rot)
-
-    def _set_target_rot(self, target_rot):
-        self.logger.info("[target] rot: %d" %target_rot)
-        self._target_rot = target_rot
-    def _get_target_rot(self):
-        return self._target_rot
-    target_rot = property(_get_target_rot, _set_target_rot)
-
-    def _set_mission(self, mission):
-        self.logger.info("[mission] %s -> %s"
-                %(self.mission, mission))
-        self._mission = mission
-    def _get_mission(self):
-        return self._mission
-    mission = property(_get_mission, _set_mission)
-
-    ### MISSIONS DISPONIBLE ###
-    
-    # avancer d'une distance donn
+    # avancer de dist
     def forward(self, callback, dist):
         '''Le fait de raisonner sur target permet de corriger les imprcisions
         de l'asserv, car target="l'endroit ou l'asserv aurait du nous ammener"'''
-        if self.mission == None:
-            self.target_pos += Vertex(dist * cos(self.rot/18000*pi), dist * sin(self.rot/18000*pi))
+        if self.fonction == None:
+            self.fonction = "forward"
             self.callback = callback
-            self.mission = "forward"
-            distance = (self.target_pos - self.pos).norm()
-            # FIXME: y'a sans doute plus simple
-            distance *=  copysign(1, (self.target_pos - self.pos)
-                    * Vertex(20*cos(self.rot/18000*pi), 20*sin(self.rot/18000*pi)))
-            self.missions["forward"].start(self, distance)
+            self.value["dist"] = dist
+            self.odo.request(self)
 
+    # s'arreter sur la droite x=...
+    def reach_x(self, callback, x):
+        if self.fonction == None:
+            self.fonction = "reach_x"
+            self.callback = callback
+            self.value["x"] = x
+            self.odo.request(self)
+
+    # s'arreter sur la droite y=...
     def reach_y(self, callback, y):
-        if self.mission == None:
+        if self.fonction == None:
+            self.fonction = "reach_y"
             self.callback = callback
-            self.mission = "forward"
-            dy = abs(y - self.pos.y)
-            dtheta = abs(self.rot)
-            dist = -dy/sin(dtheta/18000*pi)
-            print("rot: %d, target_rot: %d, dtheta: %d" %(self.rot,
-                self.target_rot, dtheta))
-            print("pos: ", self.pos)
-            print("target: ", self.target_pos)
-            self.target_pos += Vertex(dist * cos(self.rot/18000*pi), dist * sin(self.rot/18000*pi))
-            print("target: ", self.target_pos)
-            self.missions["forward"].start(self, dist)
+            self.value["y"] = y
+            self.odo.request(self)
 
-    def rotate(self, callback, angle, relative = True):
-        if self.mission == None:
+    # effectuer une rotation, relative a la position actuelle ou absolue
+    def rotate(self, callback, angle, absolute = False):
+        if self.fonction == None:
+            self.fonction = "rotate"
             self.callback = callback
-            self.mission = "rotate"
-            if relative:
-                self.target_rot += angle
-            else:
-                self.target_rot = angle
-            #print("Angle: %d" %angle)
-            realangle = angle_normalize(self.target_rot - self.rot)
-            #print("Pos: %d, Target: %d, Rotate: %d"%(self.rot, self.target_rot,
-            #    realangle))
-            if realangle > 17000 and angle < 0:
-                realangle = realangle - 36000
-            elif realangle < -17000 and angle > 0:
-                realangle = realangle + 36000
-            #print("Rectified angle: %d" %realangle)
-            self.missions["rotate"].start(self, realangle)
+            self.value["angle"] = angle
+            self.value["absolute"] = absolute
+            self.odo.request(self)
 
     def speed(self, speed, curt = False):
-        print('speed !!!!')
-        if self.mission == None:
-            print('speedyyyyy !!!!')
+        if self.fonction == None:
+            self.fonction = "speed"
             self.mission = "speed"
-            self.state = "running"
+            self.state = "run"
             self.missions["speed"].start(speed, curt)
             
-    #def speed_target(self, left, right, curt = False):
-    #    if self.mission == None:
-    #        self.mission = "speed_target"
-    #        self.state = "running"
-    #        self.missions["speed_target"].start(left, right, curt)
-
     def stop(self, callback):
-        if self.mission == "speed":
+        if self.fonction == "speed" and self.state == "run":
             self.callback = callback
             self.state = "stopping"
             self.missions["speed"].stop(callback)
 
-    ### FINDESMISSIONS ###
+    ### FIN DES FONCTIONS ###
 
     def process_event(self, event):
-        if self.mission == "forward" or self.mission == "rotate" \
-                or ((self.mission == "speed" or self.mission == "speed_target") and self.state == "stopping"):
-            if event.name == self.mission and event.type == "done":
-                if self.mission == "speed":
-                    self.target_pos += Vertex(event.value * cos(self.rot/18000*pi), event.value * sin(self.rot/18000*pi))
-                self.mission = None
-                if self.state != None: # c'est pour pas produire de log inutile
-                    self.state = None
-                self.send_event(Event("move", "done", [self.callback]))
+        if event.name == "odo" and event.type == "answer" and self.mission == None:
+            if self.fonction == "forward":
+                self.mission = "forward"
+                print("Position actuelle : %s %d" %(event.pos, event.rot))
+                print("Target actuelle : %s %d" %(self.odo.target_pos, self.odo.target_rot))
+                deplacement = Vertex(self.value["dist"] * cos(self.odo.target_rot/18000*pi), self.value["dist"] * sin(self.odo.target_rot/18000*pi))
+                print("Distance : %d" %self.value["dist"])
+                print("Vecteur de deplacement : %s" %deplacement)
+                self.odo.target_pos += deplacement
+                print("Nouvelle target : %s %d" %(self.odo.target_pos, self.odo.target_rot))
+                distance = copysign(deplacement.norm(), self.value["dist"])
+                print("Consigne : %d" %distance)
+                self.missions["forward"].start(self, distance)
+
+            elif self.fonction == "reach_x":
+                self.mission = "forward"
+                print("Position actuelle : %s %d" %(self.odo.pos, event.rot))
+                print("Consigne: x=%d" %self.value["x"])
+                dx = self.value["x"] - event.pos.x
+                dtheta = event.rot
+                dist = dx/cos(dtheta/18000*pi)
+                print("dx: %d, dtheta: %d, dist: %d" %(dx, dtheta, dist))
+                self.odo.target_pos += Vertex(dist * cos(event.rot/18000*pi), dist * sin(event.rot/18000*pi))
+                self.missions["forward"].start(self, dist)
+
+            elif self.fonction == "reach_y":
+                self.mission = "forward"
+                print("Position actuelle : %s %d" %(event.pos, event.rot))
+                print("Consigne: y=%d" %self.value["y"])
+                dy = self.value["y"] - event.pos.y
+                dtheta = event.rot
+                dist = dy/sin(dtheta/18000*pi)
+                print("dy: %d, dtheta: %d, dist: %d" %(dy, dtheta, dist))
+                self.odo.target_pos += Vertex(dist * cos(self.odo.rot/18000*pi), dist * sin(self.odo.rot/18000*pi))
+                self.missions["forward"].start(self, dist)
+
+            elif self.fonction == "rotate":
+                self.mission = "rotate"
+                if self.value["absolute"]:
+                    self.odo.target_rot = self.value["angle"]
+                else:
+                    self.odo.target_rot += self.value["angle"]
+                print("Angle: %d" %self.value["angle"])
+                realangle = angle_normalize(self.odo.target_rot - event.rot)
+                print("Pos: %d, Target: %d, Rotate: %d"%(event.rot, self.odo.target_rot, realangle))
+                if realangle > 17000 and self.angle < 0:
+                    realangle = realangle - 36000
+                elif realangle < -17000 and self.angle > 0:
+                    realangle = realangle + 36000
+                print("Rectified angle: %d" %realangle)
+                self.missions["rotate"].start(self, realangle)
+
+        elif event.name == self.mission and event.type == "done":
+            if self.mission == "speed":
+                self.odo.target_pos += Vertex(event.value * cos(self.odo.target_rot/18000*pi), event.value * sin(self.odo.target_rot/18000*pi))
+                self.state = None
+            self.fonction = None
+            self.mission = None
+            self.send_event(Event("move", "done", [self.callback]))
