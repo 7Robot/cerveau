4d8b0c6a19f4ff85c3a835f4100efbc58354ecb1
vins31 <vincentweb31@gmail.com>
2012-05-18 23:31:42 +0200

    on désactive la fin

--- ia/missions/petit/lingot_milieu.py
+++ ia/missions/petit/lingot_milieu.py
@@ -45,23 +45,23 @@ class Lingot_MilieuMission(Mission):
                 self.state += 1
                 self.missions["forward"].start(self, -12500)
                 
-        elif self.state == 6:
-            if e.name == "forward" and e.type == "done":
-                self.state += 1
-                self.missions["rotate"].start(self, 13500)
-                
-        elif self.state == 7:
-            if e.name == "rotate" and e.type == "done":
-                self.state += 1
-                self.missions["forward"].start(self, 7800)
-                
-        elif self.state == 8:
-            if e.name == "forward" and e.type == "done":
-                self.state += 1
-                self.missions["rotate"].start(self, -6000)
-                
-        elif self.state == 9:
-            if e.name == "rotate" and e.type == "done":
-                self.state += 1
-                self.missions["speed"].start(20)
+#        elif self.state == 6:
+#            if e.name == "forward" and e.type == "done":
+#                self.state += 1
+#                self.missions["rotate"].start(self, -13700)
+#                
+#        elif self.state == 7:
+#            if e.name == "rotate" and e.type == "done":
+#                self.state += 1
+#                self.missions["forward"].start(self, 7800)
+#                
+#        elif self.state == 8:
+#            if e.name == "forward" and e.type == "done":
+#                self.state += 1
+#                self.missions["rotate"].start(self, -6000)
+#                
+#        elif self.state == 9:
+#            if e.name == "rotate" and e.type == "done":
+#                self.state += 1
+#                self.missions["speed"].start(20)
                 
